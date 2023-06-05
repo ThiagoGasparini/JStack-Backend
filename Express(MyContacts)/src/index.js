11c18289e2c1257dbd6c5762affdd52113bacf3f
Thiago Gasparini <thiago_engprod@outlook.com>
2023-06-04 21:22:35 -0300

    add tratamento de erros

--- Express(MyContacts)/src/index.js
+++ Express(MyContacts)/src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
+require('express-async-errors');
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, req, res, next) => {
+  console.log(error);
+  res.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('Server Started at http://localhost:3000'));
