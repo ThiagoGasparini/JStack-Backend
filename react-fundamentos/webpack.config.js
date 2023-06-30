780b0c8a629629e6458187c54ea14c1d5357c906
Thiago Gasparini <thiago_engprod@outlook.com>
2023-06-30 17:57:25 -0300

    add setup do webpack-server

--- react-fundamentos/webpack.config.js
+++ react-fundamentos/webpack.config.js
@@ -3,15 +3,28 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'transpiled', 'index.js'),
+  entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle[hash].js',
   },
+  mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html'),
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  devServer: {
+    port: 3000,
+  }
 };
