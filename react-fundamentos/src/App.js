63822c6f30cfb2f57716746636a9fff104011dec
Thiago Gasparini <thiago_engprod@outlook.com>
2024-02-25 13:26:20 -0300

    expressões js no jsx

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react';
 
 function App() {
+  const text = 'Hello World!'
+
   return (
     <Fragment>
       <h1>Componente App</h1>
-      <h2>Subtítulo</h2>
+      <h2>{ text ? text : <h2>O texto está vazio</h2> }</h2>
     </Fragment>
-  )
+  );
 }
 
 export default App;
