96dc3254cdc6dbf4d898270b6644560fd490355c
Thiago Gasparini <thiago_engprod@outlook.com>
2024-02-25 13:45:26 -0300

    props no react

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,12 +1,20 @@
 import React, { Fragment } from 'react';
+import Post from './Post';
 
 function App() {
-  const text = 'Hello World!'
+  const text = 'Hello World!';
 
   return (
     <Fragment>
       <h1>Componente App</h1>
-      <h2>{ text ? text : <h2>O texto está vazio</h2> }</h2>
+      <h2>{text ? text : <h2>O texto está vazio</h2>}</h2>
+
+      <hr />
+
+      <Post title="Título da notícia 01" subtitle="Subtítulo da notícia 01" />
+      <Post title="Título da notícia 02" subtitle="Subtítulo da notícia 02" />
+      <Post title="Título da notícia 03" subtitle="Subtítulo da notícia 03" />
+      <Post title="Título da notícia 04" subtitle="Subtítulo da notícia 04" />
     </Fragment>
   );
 }
