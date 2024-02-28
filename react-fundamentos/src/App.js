7a084990165f4d76aa70624ad7371140f6fb9085
Thiago Gasparini <thiago_engprod@outlook.com>
2024-02-28 17:30:05 -0300

    trabalhando com estados e funções

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,28 +1,50 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Post from './Post';
 import Header from './Header';
 
 function App() {
-  const posts = [
-    { id: 1, title: 'Title#01', subtitle: 'Sub#01', likes: 20 },
-    { id: 2, title: 'Title#02', subtitle: 'Sub#02', likes: 10 },
-    { id: 3, title: 'Title#03', subtitle: 'Sub#03', likes: 50 },
-    { id: 4, title: 'Title#04', subtitle: 'Sub#04', likes: 15 },
-  ];
+  const [posts, setPosts] = useState([
+    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20 },
+    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10 },
+    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50 },
+    { id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 15 },
+  ]);
+
+  const handleRefresh = () => {
+    setPosts((prevState) => [
+      ...prevState,
+      {
+        id: Math.random(),
+        title: `Title#${prevState.length + 1}`,
+        subtitle: `Sub#${prevState.length + 1}`,
+        likes: 20,
+      },
+    ]);
+  };
+
+  const handleDelete = (id) => {
+    const deletePost = posts.filter((post) => post.id !== id);
+    setPosts(deletePost);
+  };
 
   return (
     <Fragment>
       <Header title="JStack's Blog">
-        <h2>O texto está vazio!</h2>
+        <h2>
+          O texto está vazio!
+          <button onClick={handleRefresh}>Atualizar</button>
+        </h2>
       </Header>
       <hr />
       {posts.map((post) => (
-        <Post
-          key={post.id}
-          likes={post.likes}
-          title={post.title}
-          subtitle={post.subtitle}
-        />
+        <div key={post.id}>
+          <button onClick={() => handleDelete(post.id)}>Excluir</button>
+          <Post
+            likes={post.likes}
+            title={post.title}
+            subtitle={post.subtitle}
+          />
+        </div>
       ))}
     </Fragment>
   );
