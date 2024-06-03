a3203fb9c237d96e76cafade5b8abee7489b5106
Thiago Gasparini <thiago_engprod@outlook.com>
2024-06-03 20:02:32 -0300

    refatorando alguns elementos basicos

--- MyContacts-Project/Frontend/front-mycontacts/src/pages/Home/index.js
+++ MyContacts-Project/Frontend/front-mycontacts/src/pages/Home/index.js
@@ -50,46 +50,6 @@ export default function Home() {
           </button>
         </div>
       </Card>
-
-      <Card>
-        <div className="info">
-          <div className="contact-name">
-            <strong>Thiago Gasparini</strong>
-            <small>Instagram</small>
-          </div>
-          <span>thiago_engprod@outlook.com</span>
-          <span>(41) 99704-8358</span>
-        </div>
-
-        <div className="actions">
-          <a href="/">
-            <img src={edit} alt="Edit" />
-          </a>
-          <button type="button">
-            <img src={trash} alt="Deletar" />
-          </button>
-        </div>
-      </Card>
-
-      <Card>
-        <div className="info">
-          <div className="contact-name">
-            <strong>Thiago Gasparini</strong>
-            <small>Instagram</small>
-          </div>
-          <span>thiago_engprod@outlook.com</span>
-          <span>(41) 99704-8358</span>
-        </div>
-
-        <div className="actions">
-          <a href="/">
-            <img src={edit} alt="Edit" />
-          </a>
-          <button type="button">
-            <img src={trash} alt="Deletar" />
-          </button>
-        </div>
-      </Card>
     </Container>
   );
 }
