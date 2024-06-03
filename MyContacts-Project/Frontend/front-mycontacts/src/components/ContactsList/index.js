f177f0edf97707074693f54f323a18f99155305e
Thiago Gasparini <thiago_engprod@outlook.com>
2024-06-03 18:41:39 -0300

    iniciando os estilos e imagens das listas de contatos

--- MyContacts-Project/Frontend/front-mycontacts/src/components/ContactsList/index.js
+++ MyContacts-Project/Frontend/front-mycontacts/src/components/ContactsList/index.js
@@ -1,9 +1,22 @@
-import { Container } from './styles';
+import { Container, Header, ListContainer } from './styles';
+
+import arrow from '../../assets/icons/arrow.svg';
 
 export default function ContactsList() {
   return (
     <Container>
-      ContactsList
+      <Header>
+        <strong>3 Contatos</strong>
+        <a href="/">Novo Contato</a>
+      </Header>
+      <ListContainer>
+        <header>
+          <button type="button" className="sort-button">
+            <span>Nome</span>
+            <img src={arrow} alt="arrow" />
+          </button>
+        </header>
+      </ListContainer>
     </Container>
   );
 }
