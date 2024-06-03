c45c7b343f7608d62fc0400a8e5d2894ae3a7411
Thiago Gasparini <thiago_engprod@outlook.com>
2024-06-03 19:22:34 -0300

    finalizando estilos dos cards de listas de contatos

--- MyContacts-Project/Frontend/front-mycontacts/src/components/ContactsList/index.js
+++ MyContacts-Project/Frontend/front-mycontacts/src/components/ContactsList/index.js
@@ -1,6 +1,10 @@
-import { Container, Header, ListContainer } from './styles';
+import {
+  Container, Header, ListContainer, Card,
+} from './styles';
 
 import arrow from '../../assets/icons/arrow.svg';
+import edit from '../../assets/icons/edit.svg';
+import trash from '../../assets/icons/trash.svg';
 
 export default function ContactsList() {
   return (
@@ -11,12 +15,69 @@ export default function ContactsList() {
       </Header>
       <ListContainer>
         <header>
-          <button type="button" className="sort-button">
+          <button type="button">
             <span>Nome</span>
             <img src={arrow} alt="arrow" />
           </button>
         </header>
       </ListContainer>
+      <Card>
+        <div className="info">
+          <div className="contact-name">
+            <strong>Thiago Gasparini</strong>
+            <small>Instagram</small>
+          </div>
+          <span>thiago_engprod@outlook.com</span>
+          <span>(41) 99704-8358</span>
+        </div>
+
+        <div className="actions">
+          <a href="/">
+            <img src={edit} alt="Edit" />
+          </a>
+          <button type="button">
+            <img src={trash} alt="Deletar" />
+          </button>
+        </div>
+      </Card>
+      <Card>
+        <div className="info">
+          <div className="contact-name">
+            <strong>Thiago Gasparini</strong>
+            <small>Instagram</small>
+          </div>
+          <span>thiago_engprod@outlook.com</span>
+          <span>(41) 99704-8358</span>
+        </div>
+
+        <div className="actions">
+          <a href="/">
+            <img src={edit} alt="Edit" />
+          </a>
+          <button type="button">
+            <img src={trash} alt="Deletar" />
+          </button>
+        </div>
+      </Card>
+      <Card>
+        <div className="info">
+          <div className="contact-name">
+            <strong>Thiago Gasparini</strong>
+            <small>Instagram</small>
+          </div>
+          <span>thiago_engprod@outlook.com</span>
+          <span>(41) 99704-8358</span>
+        </div>
+
+        <div className="actions">
+          <a href="/">
+            <img src={edit} alt="Edit" />
+          </a>
+          <button type="button">
+            <img src={trash} alt="Deletar" />
+          </button>
+        </div>
+      </Card>
     </Container>
   );
 }
